ded503ba780dab24cb40d9572dbf1ceded134797
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2024-12-27 00:01:36 +0900

    [SB-28] fix: lint 위반 코드 수정

--- app/src/main/java/co/kr/hoyaho/subway/MainActivity.kt
+++ app/src/main/java/co/kr/hoyaho/subway/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "hoyahozz",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
