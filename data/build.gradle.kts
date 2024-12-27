0d023af866ecb5825c85c69b5b605057f67250be
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2024-12-27 13:59:15 +0900

    [SB-29] chore: data 모듈을 안드로이드 모듈로 변경

---
+++ data/build.gradle.kts
@@ -0,0 +1,19 @@
+import co.kr.hoyahozz.subway.setNamespace
+
+plugins {
+    id("subway.android.library")
+    id("subway.android.hilt")
+    id("kotlinx-serialization")
+}
+
+android {
+    setNamespace("data")
+}
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.immutable)
+}
