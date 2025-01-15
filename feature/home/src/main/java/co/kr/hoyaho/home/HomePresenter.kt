e4062f391c1c9fe7e1ad29fc8dc17cf752f85f9a
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2025-01-16 00:23:11 +0900

    [SB-41] refactor: 홈 화면 Circuit 적용

---
+++ feature/home/src/main/java/co/kr/hoyaho/home/HomePresenter.kt
@@ -0,0 +1,53 @@
+package co.kr.hoyaho.home
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import co.kr.hoyaho.domain.usecase.GetStationsUseCase
+import com.slack.circuit.codegen.annotations.CircuitInject
+import com.slack.circuit.runtime.Navigator
+import com.slack.circuit.runtime.presenter.Presenter
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+class HomePresenter @AssistedInject constructor(
+    private val getStationsUseCase: GetStationsUseCase,
+    @ApplicationContext private val appContext: Context,
+    @Assisted private val navigator: Navigator,
+) : Presenter<HomeScreen.State> {
+    @Composable
+    override fun present(): HomeScreen.State {
+        val state by produceState<HomeScreen.State>(HomeScreen.State.Loading) {
+            val stations = getStationsUseCase().getOrElse {
+                // 더 좋은 방법이 없을까?
+                Toast.makeText(appContext, "알 수 없는 오류가 발생하였습니다.", Toast.LENGTH_SHORT).show()
+                emptyList()
+            }
+
+            value = HomeScreen.State.Idle(
+                stations = stations,
+                eventSink = { event ->
+                    when (event) {
+                        is HomeScreen.Event.OnStationClicked -> {
+                            // TODO navigate
+                            navigator
+                        }
+                    }
+                },
+            )
+        }
+
+        return state
+    }
+
+    @CircuitInject(HomeScreen::class, ActivityRetainedComponent::class)
+    @AssistedFactory
+    fun interface Factory {
+        fun create(navigator: Navigator): HomePresenter
+    }
+}
--- feature/main/src/main/java/co/kr/hoyaho/main/MainActivity.kt
+++ feature/main/src/main/java/co/kr/hoyaho/main/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import co.kr.hoyaho.designsystem.theme.SubwayTheme
-import co.kr.hoyaho.main.ui.SubwayApp
-import co.kr.hoyaho.main.ui.rememberSubwayAppState
+import co.kr.hoyaho.home.HomeScreen
+import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.Circuit
 import com.slack.circuit.foundation.CircuitCompositionLocals
+import com.slack.circuit.foundation.NavigableCircuitContent
+import com.slack.circuit.foundation.rememberCircuitNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -22,11 +27,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val appState = rememberSubwayAppState()
+            val backStack = rememberSaveableBackStack(root = HomeScreen)
+            val navigator = rememberCircuitNavigator(backStack)
 
             SubwayTheme {
                 CircuitCompositionLocals(circuit) {
-                    SubwayApp(appState)
+                    Scaffold { padding ->
+                        NavigableCircuitContent(
+                            navigator = navigator,
+                            backStack = backStack,
+                            modifier = Modifier.padding(padding),
+                        )
+                    }
                 }
             }
         }
